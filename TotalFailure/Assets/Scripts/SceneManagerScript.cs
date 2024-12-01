ebf75fbfba9071e7595d9bcd497b7ca0b1bc91bb
Rostrost3 <rostikmakayda@outlook.com>
2024-12-01 14:43:13 +0300

    Edited MainMenu

--- TotalFailure/Assets/Scripts/SceneManagerScript.cs
+++ TotalFailure/Assets/Scripts/SceneManagerScript.cs
@@ -2,9 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Rendering;
+using TMPro;
 
 public class SceneManagerScript : MonoBehaviour
 {
+    public TMP_Dropdown resolutionDropdown;
+    Resolution[] resolutions;
+
+    [SerializeField] Slider volumeSlider = null;
+    [SerializeField] TextMeshProUGUI volumeTextUI = null;
+
+    private void Start()
+    {
+        resolutionDropdown.ClearOptions();
+        List<string> options = new List<string>();
+        resolutions = Screen.resolutions;
+        int currentResolutionsIndex = 0;
+
+        for(int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height + " "  + resolutions[i].refreshRateRatio + "Hz";
+            options.Add(option);
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionsIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.RefreshShownValue();
+        LoadSettings(currentResolutionsIndex);
+
+        volumeSlider.onValueChanged.AddListener((volume) =>
+        {
+            volumeTextUI.text = volume.ToString("0");
+        });
+    }
 
     public void GameStart()
     {
@@ -19,5 +54,43 @@ public class SceneManagerScript : MonoBehaviour
     public void GameExit()
     {
         Application.Quit();
+    }
+
+    public void SetFullScreen(bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    public void SaveSettings()
+    {
+        PlayerPrefs.SetInt("ResolutionPreference", resolutionDropdown.value);
+        PlayerPrefs.SetInt("fullScreenPreference", System.Convert.ToInt32(Screen.fullScreen));
+    }
+
+    public void LoadSettings(int currentResolutionIndex)
+    {
+        if (PlayerPrefs.HasKey("ResolutionPreference"))
+        {
+            resolutionDropdown.value = PlayerPrefs.GetInt("ResolutionPreference");
+        }
+        else
+        {
+            resolutionDropdown.value = currentResolutionIndex;
+        }
+
+        if (PlayerPrefs.HasKey("fullScreenPreference"))
+        {
+            Screen.fullScreen = System.Convert.ToBoolean(PlayerPrefs.GetInt("fullScreenPreference"));
+        }
+        else
+        {
+            Screen.fullScreen = true;
+        }
     }
 }
